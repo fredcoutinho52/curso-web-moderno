fdafeda13854878dcb5936732d05899751d57db6
fredcoutinho52 <fredcoutinho52@gmail.com>
2020-01-28 18:03:56 -0300

    Módulo de funções iniciado

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,35 @@
+// Função em JS é First-Class Object (Citizens) => Trata a função como um dado
+// High-order function
+
+// Criar de forma literal
+function fun1() { }
+
+// Armazenar em uma variável
+const fun2 = function() { }
+
+// Armazenar em um array
+const array = [function (a, b) { return a + b }, fun1, fun2]
+console.log(array[0](2, 3))
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function() { return 'Opa' }
+console.log(obj.falar())
+
+// Passar função como parâmetro de outra função
+function run(fun) {
+    fun()
+}
+
+run(function() { console.log('Executando...') })
+
+// Função pode retornar/conter uma função
+function soma(a, b) {
+    return function(c) {
+        console.log(a + b + c)
+    }
+}
+
+soma(2, 3)(4)
+const cincoMais = soma(2, 3)
+cincoMais(4)
