7742af098350445894db4268ddd52e000656ced1
Fred Coutinho <fredcoutinho52@gmail.com>
2020-05-04 11:05:09 -0300

    Array chapter finished

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Vesclei']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log(['a', 'b', 'c'].concat([1, 2], [3, 4], 5, [[6, 7]]))
